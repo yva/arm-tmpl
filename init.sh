8874044d84c64a2f39b1c7af18e1d0be5c8339b0
AlexanderM <alexmak@findo.com>
2019-03-11 19:07:42 +0300

    fix init.sh

--- init.sh
+++ init.sh
@@ -14,10 +14,6 @@ done
 ENV_64BASE=$1
 ENV_DECODED=$(echo "$ENV_64BASE" | base64 --decode)
 
-defUpdateURL='https://dl.yva.ai/public/releases/yva/release/yva.vm.json'
-EXT_PLATFORM_INIT_KV_OVERLOAD="{  \"kv\": { \"PLATFORM_UPDATE_URL\": \"${1:-${PLATFORM_UPDATE_URL:-$defUpdateURL}}\" } }"
-PLATFORM_INIT_KV_OVERLOAD=$( ( echo "$EXT_PLATFORM_INIT_KV_OVERLOAD"; echo "${PLATFORM_INIT_KV_OVERLOAD:-{\}}" | jq -cer '.') | jq -ser '.[0] * .[1]' )
-
 upurl=$(echo "$ENV_DECODED" | jq -cer '.kv.PLATFORM_UPDATE_URL')
 zipurl=$(curl -sSf "$upurl" | jq -cre '.assets.setup.source.url')
 
@@ -27,5 +23,5 @@ dir=$(mktemp -d -p "$DIR")
 pushd "$dir"
 curl -sSf "$zipurl" -o "$dir/setup.zip"
 unzip setup.zip 
-./azure.sh
+./azure.sh "$ENV_DECODED"
 popd
