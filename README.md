4061e4bbc4cd46c77cb89d59698abe64563285d5
AlexanderM <alexmak@findo.com>
2019-03-26 20:43:03 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ARM templates for YVA azure deploy
+
+[![Deploy to Azure](https://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
+
+## Template variables  
+
+* **name**: prefix of azure generated url 4 your Yva instance, for example *aztmp.westeurope.cloudapp.azure.com*, **name** is *aztmp*
+* **yvalogin** & **yvapass**: credentials you recived from our sell department
+* **securestring**: password you provide 4 access to admin page 
+
+Please visit [yva.ai](https://yva.ai) for details
---
+++ yva/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu -o pipefail
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+export DEBIAN_FRONTEND=noninteractive
+
+#install packets
+apt-get update -y
+for p in wget jq unzip curl; do
+  for i in $(seq 0 10); do apt-get install -y "$p" && break; sleep 10s; apt-get update -y;  done
+done
+
+# decode settings
+ENV_64BASE=$1
+ENV_DECODED=$(echo "$ENV_64BASE" | base64 --decode)
+echo "$ENV_DECODED" > $(mktemp -p "$DIR" -t "debug.kv.XXXXXX.json")
+
+
+upurl=$(echo "$ENV_DECODED" | jq -cer '.kv.PLATFORM_UPDATE_URL')
+zipurl=$(curl -sSf "$upurl" | jq -cre '.assets.setup.source.url')
+
+echo "Yva install: use >>$upurl<< and >>$zipurl<<." 1>&2
+dir=$(mktemp -d)
+pushd "$dir"
+curl -sSf "$zipurl" -o "$dir/setup.zip"
+unzip setup.zip 
+./azure.sh "$ENV_DECODED"
+popd
