96035dea561e501376137ad3d0567a58aba86b11
AlexanderM <alexmak@findo.com>
2019-06-06 00:17:40 +0300

    update retry polisy

--- yva/init.sh
+++ yva/init.sh
@@ -5,16 +5,19 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 export DEBIAN_FRONTEND=noninteractive
 
 #install packets, make retrys on update it sometimes fails at azure
-for i in $(seq 0 10); do apt-get update -y && break; sleep 10; done
+apt-get update -y
 for p in wget jq unzip curl; do
-  for i in $(seq 0 10); do apt-get install -y "$p" && break; sleep 10s; done
+  for i in $(seq 0 10); do 
+    apt-get install -y "$p" && break;
+    sleep 10s
+    apt-get update -y
+  done
 done
 
 # decode settings
 ENV_64BASE=$1
 ENV_DECODED=$(echo "$ENV_64BASE" | base64 --decode)
 echo "$ENV_DECODED" > $(mktemp -p "$DIR" -t "debug.kv.XXXXXX.json")
-
 
 upurl=$(echo "$ENV_DECODED" | jq -cer '.kv.PLATFORM_UPDATE_URL')
 zipurl=$(curl -sSf "$upurl" | jq -cre '.assets.setup.source.url')
